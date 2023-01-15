28a655040f20cb8d15f68b1ca4893b95aa4f449f
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2023-01-15 18:32:21 +0300

    Changes for thymeleaf

--- src/main/java/com/company/controllers/AdminController.java
+++ src/main/java/com/company/controllers/AdminController.java
@@ -5,11 +5,12 @@ import com.company.models.food.Food;
 import com.company.models.food.FoodType;
 import com.company.services.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("admin")
 public class AdminController {
   private final FoodService foodService;
--- src/main/java/com/company/controllers/CommonController.java
+++ src/main/java/com/company/controllers/CommonController.java
@@ -3,25 +3,34 @@ package com.company.controllers;
 import com.company.models.food.FoodType;
 import com.company.repositories.food.FoodTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/")
-public class Controller {
+public class CommonController {
   private final FoodTypeRepository foodTypeRepository;
 
   @Autowired
-  public Controller(FoodTypeRepository foodTypeRepository) {
+  public CommonController(FoodTypeRepository foodTypeRepository) {
     this.foodTypeRepository = foodTypeRepository;
   }
 
+  @GetMapping(path = "index")
+  public String index(Model model) {
+    return "redirect:/sir";
+  }
+
   @GetMapping(path = "sir")
-  public String sir() {
-    return "SIR, YES SIR!";
+  public String sir(Model model) {
+    model.getAttribute("A");
+    model.addAttribute("A", "valla");
+
+    return "abc";
   }
 
   @GetMapping(path = "menu")
