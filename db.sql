1b01da4adba9803a47b2c4f412a416df4083a934
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2023-01-25 17:29:06 +0300

    Add NOT NULL

--- db.sql
+++ db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "people"
 (
     "id"       serial PRIMARY KEY,
-    "name"     varchar,
-    "birthday" timestamp,
-    "hp"       int,
-    "mana"     int,
-    "stamina"  int,
-    "alcohol" int,
+    "name"     varchar NOT NULL,
+    "birthday" timestamp NOT NULL,
+    "hp"       int NOT NULL,
+    "mana"     int NOT NULL,
+    "stamina"  int NOT NULL,
+    "alcohol"  int NOT NULL,
     CHECK (
                 hp >= 0
             AND mana >= 0
@@ -18,9 +18,9 @@ CREATE TABLE "people"
 CREATE TABLE "loyalty_levels"
 (
     "level"       serial PRIMARY KEY,
-    "sale"        int,
-    "description" varchar,
-    "money"       int,
+    "sale"        int NOT NULL,
+    "description" varchar NOT NULL,
+    "money"       int NOT NULL,
     CHECK (sale >= 0 AND sale <= 100)
 );
 
@@ -35,8 +35,8 @@ CREATE TABLE "ban_records"
 (
     "id"          serial PRIMARY KEY,
     "customer_id" int UNIQUE REFERENCES "customers" ON UPDATE CASCADE ON DELETE CASCADE,
-    "from"        timestamp,
-    "to"          timestamp
+    "from"        timestamp NOT NULL,
+    "to"          timestamp NOT NULL
 );
 
 -- TABLE "professions" -> enum Profession
@@ -44,7 +44,7 @@ CREATE TABLE "ban_records"
 CREATE TABLE "workers"
 (
     "id"         int PRIMARY KEY REFERENCES "people" ON UPDATE CASCADE ON DELETE CASCADE,
-    "profession" varchar
+    "profession" varchar NOT NULLk
 );
 
 
@@ -55,11 +55,11 @@ CREATE TABLE "workers"
 CREATE TABLE "food_types"
 (
     "id"      serial PRIMARY KEY,
-    "name"    varchar UNIQUE,
-    "price"   int,
-    "hp"      int,
-    "mana"    int,
-    "stamina" int,
+    "name"    varchar UNIQUE NOT NULL,
+    "price"   int NOT NULL,
+    "hp"      int NOT NULL,
+    "mana"    int NOT NULL,
+    "stamina" int NOT NULL,
     CHECK (
                 hp >= 0
             AND mana >= 0
@@ -69,15 +69,15 @@ CREATE TABLE "food_types"
 
 CREATE TABLE "mead_types"
 (
-    "id"        int PRIMARY KEY REFERENCES "food_types" ON UPDATE CASCADE ON DELETE CASCADE,
-    "alcohol"   int,
+    "id"      int PRIMARY KEY REFERENCES "food_types" ON UPDATE CASCADE ON DELETE CASCADE,
+    "alcohol" int NOT NULL,
     CHECK (alcohol > 0 AND alcohol < 100)
 );
 
 CREATE TABLE "food_storage"
 (
     "food_type" int PRIMARY KEY REFERENCES "food_types" ON UPDATE CASCADE ON DELETE CASCADE,
-    "count"     int,
+    "count"     int NOT NULL,
     CHECK (count > 0)
 );
 
@@ -85,15 +85,15 @@ CREATE TABLE "orders"
 (
     "id"          serial PRIMARY KEY,
     "customer_id" int REFERENCES "customers" ON UPDATE CASCADE ON DELETE CASCADE,
-    "time"        timestamp,
-    "closed"      boolean
+    "time"        timestamp NOT NULL,
+    "closed"      boolean NOT NULL
 );
 
 CREATE TABLE "order_details"
 (
     "order_id" int REFERENCES "orders" ON UPDATE CASCADE ON DELETE CASCADE,
     "food_id"  int REFERENCES "food_types" ON UPDATE CASCADE ON DELETE CASCADE,
-    "count"    int,
+    "count"    int NOT NULL,
     PRIMARY KEY ("order_id", "food_id"),
     CHECK ("count" > 0)
 );
@@ -112,45 +112,47 @@ CREATE TABLE "sessions"
 );
 
 
-CREATE FUNCTION count_spent_money_by_customer(cust_id int) RETURNS int AS $count_spent_money_by_customer$
+CREATE FUNCTION count_spent_money_by_customer(cust_id int) RETURNS int AS
+$count_spent_money_by_customer$
 DECLARE
-money_count int;
+    money_count int;
 BEGIN
-SELECT sum(od.count * ft.price)
-INTO money_count
-FROM "orders" o
-         JOIN order_details od on o.id = od.order_id
-         JOIN food_types ft on ft.id = od.food_id
-WHERE o.customer_id = cust_id
-  AND o.closed;
-RETURN money_count;
+    SELECT sum(od.count * ft.price)
+    INTO money_count
+    FROM "orders" o
+             JOIN order_details od on o.id = od.order_id
+             JOIN food_types ft on ft.id = od.food_id
+    WHERE o.customer_id = cust_id
+      AND o.closed;
+    RETURN money_count;
 END;
 $count_spent_money_by_customer$ LANGUAGE plpgsql;
 
-CREATE FUNCTION update_loyalty_level() RETURNS trigger AS $update_loyalty_level$
+CREATE FUNCTION update_loyalty_level() RETURNS trigger AS
+$update_loyalty_level$
 DECLARE
-sum     int;
+    sum     int;
     new_lvl int;
     old_lvl int;
 BEGIN
     IF NEW.closed = true THEN
         sum := count_spent_money_by_customer(NEW.customer_id);
-SELECT max(ll.level)
-INTO new_lvl
-FROM loyalty_levels ll
-WHERE ll.money <= sum;
+        SELECT max(ll.level)
+        INTO new_lvl
+        FROM loyalty_levels ll
+        WHERE ll.money <= sum;
 
-SELECT c.loyalty_level
-INTO old_lvl
-FROM customers c
-WHERE c.id = NEW.customer_id;
+        SELECT c.loyalty_level
+        INTO old_lvl
+        FROM customers c
+        WHERE c.id = NEW.customer_id;
 
-IF old_lvl != new_lvl THEN
-UPDATE customers SET loyalty_level = new_lvl WHERE id = NEW.customer_id;
-END IF;
-END IF;
+        IF old_lvl != new_lvl THEN
+            UPDATE customers SET loyalty_level = new_lvl WHERE id = NEW.customer_id;
+        END IF;
+    END IF;
     NEW.time := current_timestamp;
-RETURN NEW;
+    RETURN NEW;
 END;
 $update_loyalty_level$ LANGUAGE plpgsql;
 
@@ -158,9 +160,10 @@ CREATE TRIGGER update_loyalty_level
     BEFORE UPDATE
     ON orders
     FOR ROW
-    EXECUTE PROCEDURE update_loyalty_level();
+EXECUTE PROCEDURE update_loyalty_level();
 
-CREATE FUNCTION reduce_food_count_in_storage() RETURNS trigger AS $reduce_food_count_in_storage$
+CREATE FUNCTION reduce_food_count_in_storage() RETURNS trigger AS
+$reduce_food_count_in_storage$
 DECLARE
     count_diff int;
     old_count  int;
@@ -176,7 +179,7 @@ BEGIN
         UPDATE food_storage SET count = new_count WHERE food_type = NEW.food_id;
     END IF;
 
-RETURN NEW;
+    RETURN NEW;
 END;
 $reduce_food_count_in_storage$ LANGUAGE plpgsql;
 
@@ -184,13 +187,14 @@ CREATE TRIGGER reduce_food_count_in_storage
     BEFORE INSERT
     ON order_details
     FOR ROW
-    EXECUTE PROCEDURE reduce_food_count_in_storage();
+EXECUTE PROCEDURE reduce_food_count_in_storage();
 
-CREATE FUNCTION reduce_food_count_in_storage_by_diff() RETURNS trigger AS $reduce_food_count_in_storage_by_diff$
+CREATE FUNCTION reduce_food_count_in_storage_by_diff() RETURNS trigger AS
+$reduce_food_count_in_storage_by_diff$
 DECLARE
-    count_diff int;
-    old_count_in_storage  int;
-    new_count_in_storage  int;
+    count_diff           int;
+    old_count_in_storage int;
+    new_count_in_storage int;
 BEGIN
     count_diff := NEW.count - OLD.count;
     IF count_diff < 0 THEN
@@ -205,7 +209,7 @@ BEGIN
         UPDATE food_storage SET count = new_count_in_storage WHERE food_type = NEW.food_id;
     END IF;
 
-RETURN NEW;
+    RETURN NEW;
 END;
 $reduce_food_count_in_storage_by_diff$ LANGUAGE plpgsql;
 
@@ -213,15 +217,15 @@ CREATE TRIGGER reduce_food_count_in_storage_by_diff
     BEFORE UPDATE
     ON order_details
     FOR ROW
-    EXECUTE PROCEDURE reduce_food_count_in_storage_by_diff();
+EXECUTE PROCEDURE reduce_food_count_in_storage_by_diff();
 
 CREATE FUNCTION check_closed_from_false_to_true() RETURNS trigger AS
-    $check_closed_from_false_to_true$
+$check_closed_from_false_to_true$
 BEGIN
     IF NEW.closed = false AND OLD.closed = true THEN
         RAISE EXCEPTION 'can not open closed order';
-END IF;
-RETURN NEW;
+    END IF;
+    RETURN NEW;
 END;
 $check_closed_from_false_to_true$ LANGUAGE plpgsql;
 
@@ -229,6 +233,7 @@ CREATE TRIGGER check_closed_from_false_to_true
     BEFORE UPDATE
     ON orders
     FOR ROW
-    EXECUTE PROCEDURE check_closed_from_false_to_true();
+EXECUTE PROCEDURE check_closed_from_false_to_true();
 
-INSERT INTO loyalty_levels (level, sale, description, money) VALUES (0, 0, 'Чушка', 0);
+INSERT INTO loyalty_levels (level, sale, description, money)
+VALUES (0, 0, 'Чушка', 0);
